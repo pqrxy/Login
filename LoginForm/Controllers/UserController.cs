5b65401f5d8f289c082b2b758e909c397c5a4014
manideep.n15 <nmanideep1510@gmail.com>
2018-09-10 13:17:03 +0530

    commited

---
+++ LoginForm/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using LoginForm.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace LoginForm.Controllers
+{
+    public class UserController : Controller
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select UserName,Password from UserInfo where UserName=@name and Password=@password", con);
+                cmd.Parameters.AddWithValue("@name", u.UserName);
+                cmd.Parameters.AddWithValue("@password", u.Password);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                con.Close();
+                if (dt.Rows.Count > 0)
+                {
+                    return View("DashBoard");
+                }
+                else
+                {
+                    ViewBag.error = "Invalid UserName or Password";
+                    return View("Login");
+                }
+            }
+         else
+            {
+                return View("Login");
+            }
+                
+         }
+    }
+}
---
+++ LoginForm/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace LoginForm.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage ="enter username")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage ="enter password")]
+        public string Password { get; set; }
+    }
+}
